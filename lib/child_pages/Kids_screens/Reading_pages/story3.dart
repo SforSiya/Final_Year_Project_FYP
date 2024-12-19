29d9385c04bbce0530d1e9ad8f8254de79907229
sforsiya <swairaansar29@gmail.com>
2024-12-19 17:42:53 +0500

    5th commit

--- lib/child_pages/Kids_screens/Reading_pages/story3.dart
+++ lib/child_pages/Kids_screens/Reading_pages/story3.dart
@@ -7,15 +7,15 @@ class Story3Page extends StatelessWidget {
     {'image': 'assets/tables/table_3_4.png'},
     {'image': 'assets/tables/table_5_6.png'},
     {'image': 'assets/tables/table_7_8.png'},
-    {'image': 'assets/tables/table_9_10.png'},
-    {'image': 'assets/tables/table_11_12.png'},
+    {'image': 'assets/tables/table_9_and_10.png'},
+    {'image': 'assets/tables/table_11_and_12.png'},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Read a Clock'),
+        title: const Text('Tables'),
         backgroundColor: const Color(0xFFffde59),
       ),
       body: Padding(
@@ -43,7 +43,7 @@ class Story3Page extends StatelessWidget {
                   borderRadius: const BorderRadius.all(Radius.circular(12)),
                   child: Image.asset(
                     storyItem['image']!,
-                    height: 600, // Adjust height for a more rectangular shape
+                    height: 400, // Adjust height for a more rectangular shape
                     width: double.infinity,
                     fit: BoxFit.cover, // Ensures image fills the space
                   ),
